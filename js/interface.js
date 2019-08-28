4b1dd16bd49ec3ed47f1ad46b6d1c073f5b30cd5
zackwong <hzzzoo@126.com>
2019-08-28 11:37:02 +0800

    up

--- js/interface.js
+++ js/interface.js
@@ -269,34 +269,6 @@
 
 
 
-	/*-------------------------------------------------------------------------------
-	  Gallery
-	-------------------------------------------------------------------------------*/
-
-
-
-	$('.js-projects-gallery').each(function(){
-		$(this).magnificPopup({
-			delegate: 'a',
-		    type: 'image',
-		    removalDelay: 300,
-		    tLoading: 'Loading image #%curr%...',
-		    gallery: {
-		       enabled: true,
-		       navigateByImgClick: true,
-		       preload:[0,1]
-		    },
-		    image: {
-		       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-		       titleSrc: function(item) {
-		          return item.el.attr('title') + '<small></small>';
-		       }
-		    }
-
-		});
-	});
-
-
 
 	/*-------------------------------------------------------------------------------
 	  Ajax Form
