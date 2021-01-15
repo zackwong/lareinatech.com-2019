5fcb6080df16d513fa47efd150e977b1d7c21827
zackwong <hzzzoo@126.com>
2021-01-15 16:30:44 +0800

    up

--- js/interface.js
+++ js/interface.js
@@ -267,34 +267,3 @@
 		responsiveRefreshRate:0
 	});
 
-
-
-
-	/*-------------------------------------------------------------------------------
-	  Ajax Form
-	-------------------------------------------------------------------------------*/
-
-
-
-	if ($('.js-ajax-form').length) {
-		$('.js-ajax-form').each(function(){
-			$(this).validate({
-				errorClass: 'error wobble-error',
-			    submitHandler: function(form){
-		        	$.ajax({
-			            type: "POST",
-			            url:"mail.php",
-			            data: $(form).serialize(),
-			            success: function() {
-		                	$('.col-message, .success-message').show();
-		                },
-
-		                error: function(){
-			                $('.col-message, .error-message').show();
-			            }
-			        });
-			    }
-			});
-		});
-	}
-})(jQuery);
